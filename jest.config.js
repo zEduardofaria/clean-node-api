5060bfd73c722f528b2c1d94fa6bcbc2d2299411
Eduardo Faria <z.eduardofaria@gmail.com>
2022-06-27 14:06:57 -0300

    chore: remove unused husky file

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,10 @@
 module.exports = {
-  // roots: ['<rootDir>/tests'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**'
-  ],
+  roots: ['<rootDir>/src'],
+  // collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  },
-  moduleNameMapper: {
-    '@/tests/(.*)': '<rootDir>/tests/$1',
-    '@/(.*)': '<rootDir>/src/$1'
   }
-}
+};
