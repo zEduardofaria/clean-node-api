1b6126ffc3ef6283330b92012e87307a12310850
Eduardo Faria <z.eduardofaria@gmail.com>
2022-06-28 09:28:57 -0300

    test: ensure EmailValidatorAdapter call validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -20,4 +20,11 @@ describe('EmailValidator Adapter', () => {
     const isValid = sut.isValid('valid_email@email.com');
     expect(isValid).toBe(true);
   });
+
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter();
+    const isEmailSpy = jest.spyOn(validator, 'isEmail');
+    sut.isValid('valid_email@email.com');
+    expect(isEmailSpy).toHaveBeenCalledWith('valid_email@email.com');
+  });
 });
