9c9d21920ea641443e3132598f269c05af877fad
Eduardo Faria <z.eduardofaria@gmail.com>
2022-07-12 16:22:35 -0300

    feat: ensure LogControllerDecorator returns the same result of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -11,7 +11,7 @@ const makeController = (): Controller => {
       const httpResponse: HttpResponse = {
         statusCode: 200,
         body: {
-          name: 'Rodrigo'
+          name: 'Eduardo'
         }
       };
 
@@ -54,5 +54,25 @@ describe('LogController Decorator', () => {
 
     await sut.handle(httpRequest);
     expect(handleSpy).toHaveBeenCalledWith(httpRequest);
+  });
+
+  test('Should return the same result of the controller', async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Eduardo'
+      }
+    });
   });
 });
