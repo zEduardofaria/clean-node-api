b260b0ffa3b8ab3b0831ce5aef40a9787b823c08
Eduardo Faria <z.eduardofaria@gmail.com>
2022-07-25 14:35:57 -0300

    refactor: change file import to jest recognize it

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -5,6 +5,7 @@ import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter';
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account';
 import { Controller } from '@/presentation/protocols';
 import { LogControllerDecorator } from '../decorators/log';
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log';
 
 export const makeSignUpController = (): Controller => {
   const salt = 12;
@@ -16,6 +17,7 @@ export const makeSignUpController = (): Controller => {
     emailValidatorAdapter,
     dbAddAccount
   );
+  const logErrorRepository = new LogMongoRepository();
 
-  return new LogControllerDecorator(signUpController, LogErrorRepository);
+  return new LogControllerDecorator(signUpController, logErrorRepository);
 };
