62282325cebf7b2089bdcfefac8bdec5041d718d
Eduardo Faria <z.eduardofaria@gmail.com>
2022-08-02 21:03:09 -0300

    refactor: move validation to protocols folder

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../protocols';
-export * from '../../helpers/validators/validation';
+export * from '../../protocols/validation';
 export * from '../../../domain/usecases';
 export * from '../../../domain/models';
