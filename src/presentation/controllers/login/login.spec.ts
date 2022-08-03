c6e87436ed96fa1f7a62f08c6ffbda6438a7c1fc
Eduardo Faria <z.eduardofaria@gmail.com>
2022-08-02 21:11:07 -0300

    refactor: change Authentication to receive a model

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -6,7 +6,12 @@ import {
   unauthorized
 } from '../../helpers/http/http-helper';
 import { MissingParamError } from '../../../presentation/errors';
-import { HttpRequest, Authentication, Validation } from './login-protocols';
+import {
+  HttpRequest,
+  Authentication,
+  Validation,
+  AuthenticationModel
+} from './login-protocols';
 
 const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
@@ -20,7 +25,7 @@ const makeValidation = (): Validation => {
 
 const makeAuthentication = (): Authentication => {
   class AuthenticationStub implements Authentication {
-    async auth(email: string, password: string): Promise<string> {
+    async auth(authentication: AuthenticationModel): Promise<string> {
       return new Promise(resolve => resolve('any_token'));
     }
   }
@@ -66,7 +71,10 @@ describe('Login Controller', () => {
     const { sut, authenticationStub } = makeSut();
     const authSpy = jest.spyOn(authenticationStub, 'auth');
     await sut.handle(makeFakeRequest());
-    expect(authSpy).toHaveBeenCalledWith('any_email@mail.com', 'any_password');
+    expect(authSpy).toHaveBeenCalledWith({
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    });
   });
 
   test('Should return 401 if invalid credentials are provided', async () => {
