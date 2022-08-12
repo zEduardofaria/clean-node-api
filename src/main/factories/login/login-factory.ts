0cc30adcc7b461b33ae3dffabd49ae022b2c5c7b
Eduardo Faria <z.eduardofaria@gmail.com>
2022-08-12 14:30:14 -0300

    feat: add login route

--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -1,13 +1,13 @@
 import env from '../../config/env';
-import { BcryptAdapter } from '@/infra/criptography/bcrypt-adapter/bcrypt-adapter';
-import { JwtAdapter } from '@/infra/criptography/jwt-adapter/jwt-adapter';
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter';
+import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter';
 import { Controller } from '../../../presentation/protocols';
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository';
-import { DbAuthentication } from '@/data/usecases/authentication/db-authentication';
-import { LogMongoRepository } from '@/infra/db/mongodb/log/log-mongo-repository';
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication';
+import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository';
 import { makeLoginValidation } from './login-validation-factory';
-import { LoginController } from '@/presentation/controllers/login/login-controller';
-import { LogControllerDecorator } from '@/main/decorators/log-controller-decorator';
+import { LoginController } from '../../../presentation/controllers/login/login-controller';
+import { LogControllerDecorator } from '../../../main/decorators/log-controller-decorator';
 
 export const makeLoginController = (): Controller => {
   const salt = 12;
