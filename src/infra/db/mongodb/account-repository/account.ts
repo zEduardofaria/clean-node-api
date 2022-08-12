5a1163e9ff4acf68b911d4cdb87a37990b1e9b3d
Eduardo Faria <z.eduardofaria@gmail.com>
2022-08-11 21:55:31 -0300

    feat: ensure AccountMongoRepository retusna an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,9 +1,12 @@
 import { AddAccountRepository } from '@/data/protocols/db/add-account-repository';
+import { LoadAccountByEmailRepository } from '@/data/protocols/db/load-account-by-email-repository';
 import { AccountModel } from '@/domain/models';
 import { AddAccountModel } from '@/domain/usecases';
 import { MongoHelper } from '../helpers/mongo-helper';
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository
+  implements AddAccountRepository, LoadAccountByEmailRepository
+{
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts');
 
@@ -13,5 +16,13 @@ export class AccountMongoRepository implements AddAccountRepository {
     const accountById = await accountCollection.findOne({ _id: id });
 
     return MongoHelper.map(accountById);
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+
+    const account = await accountCollection.findOne({ email });
+
+    return account && MongoHelper.map(account);
   }
 }
