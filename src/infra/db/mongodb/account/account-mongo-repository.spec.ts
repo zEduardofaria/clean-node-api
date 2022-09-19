60dc7cc26cf97d782c588d19c4bc62707624593a
Eduardo Faria <z.eduardofaria@gmail.com>
2022-09-19 15:47:41 -0300

    test: ensure AccountMongorepository find an account by token with role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -106,12 +106,23 @@ describe('AccountMongorepository', () => {
       expect(account.password).toBe('any_password');
     });
 
-    // test('Should return null if loadByToken fails', async () => {
-    //   const sut = makeSut();
+    test('Should return an account on loadByToken with role', async () => {
+      const sut = makeSut();
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token',
+        role: 'any_role'
+      });
 
-    //   const account = await sut.loadByToken('any_email@mail.com');
+      const account = await sut.loadByToken('any_token', 'any_role');
 
-    //   expect(account).toBeFalsy();
-    // });
+      expect(account).toBeTruthy();
+      expect(account.id).toBeTruthy();
+      expect(account.name).toBe('any_name');
+      expect(account.email).toBe('any_email@mail.com');
+      expect(account.password).toBe('any_password');
+    });
   });
 });
