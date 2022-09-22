1dec351be33f1be9737de04f2156cbcad677841e
Eduardo Faria <z.eduardofaria@gmail.com>
2022-09-22 20:03:58 -0300

    feat: ensure DbSaveSurveyResult returns a SurveyResult on success

--- src/data/usecases/save-survey-result/db-save-survey-result.ts
+++ src/data/usecases/save-survey-result/db-save-survey-result.ts
@@ -11,7 +11,6 @@ export class DbSaveSurveyResult implements SaveSurveyResult {
   ) {}
 
   async save(data: SaveSurveyResultModel): Promise<SurveyResultModel> {
-    await this.saveSurveyResultRepository.save(data);
-    return null;
+    return await this.saveSurveyResultRepository.save(data);
   }
 }
